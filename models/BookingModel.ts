c8eb93c0e7b8f710022cb6a1e35b9abab69de9d0
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-06 00:29:41 +0200

    update booking model

--- models/BookingModel.ts
+++ models/BookingModel.ts
@@ -4,9 +4,10 @@ const BookingModel = new Schema({
   firstName: { type: String, require: true },
   lastName: { type: String, require: true },
   email: { type: String, require: true },
-  time: { type: Number, require: true },
-  persons: { type: String, require: true },
-  table: { type: Schema.Types.ObjectId, ref: 'Table' },
+  date: { type: String, require: true },
+  time: { type: String, require: true },
+  persons: { type: Number, require: true },
+  tableNumber: { type: Number, require: true },
 });
 
 export default mongoose.model('Booking', BookingModel, 'bookings');
